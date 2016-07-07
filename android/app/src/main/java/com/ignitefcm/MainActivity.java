5f76c6fc7a352ceaf1cf1912fc0543f1d8862126
Ahmed Abbas <ahmed1490@gmail.com>
2016-07-07 22:45:32 +0200

    ignited new proj

---
+++ android/app/src/main/java/com/ignitefcm/MainActivity.java
@@ -0,0 +1,19 @@
+package com.ignitefcm;
+
+import com.facebook.react.ReactActivity;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.airbnb.android.react.maps.MapsPackage;
+import com.i18n.reactnativei18n.ReactNativeI18n;
+import com.learnium.RNDeviceInfo.RNDeviceInfo;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "igniteFCM";
+    }
+}
